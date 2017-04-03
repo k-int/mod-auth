a0c1b7847411242152a637b8b5d55d65a845e5a3
Marc Johnson <marc@k-int.co.uk>
2017-04-03 15:20:19 +0100

    Create user in permissions module for demo

--- create-admin-user.sh
+++ create-admin-user.sh
@@ -2,16 +2,23 @@
 
 tenant_id=${1:-demo_tenant}
 
+echo "Creating admin user"
 credentials_json=$(cat ./admin-credentials.json)
 
 credentials_json="${credentials_json/tenantidhere/$tenant_id}"
 
-echo "Creating admin user"
 curl -w '\n' -X POST -D - \
      -H "Content-type: application/json" \
      -H "X-Okapi-Tenant: ${tenant_id}" \
      -d "${credentials_json}" \
      http://localhost:9130/authn/credentials
 
-#echo "Bootstrapping admin user permissions"
+echo "Bootstrapping admin user permissions"
 
+permissions_user_json=$(cat ./permission_user.json)
+
+curl -w '\n' -X POST -D - \
+     -H "Content-type: application/json" \
+     -H "X-Okapi-Tenant: ${tenant_id}" \
+     -d "${credentials_json}" \
+     http://localhost:9130/perms/users
